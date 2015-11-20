50aa9a4b5b33a6bb23abd8214f357658b2eaab48
gcschmit <gcschmit@gmail.com>
2015-11-20 10:35:02 -0600

    Started on array algorithms.
    Need to start with inserting into a partially filled array.

---
+++ Chapter 7 Practice/PartialArray.java
@@ -0,0 +1,20 @@
+public class PartialArray
+{
+    private int[] values;
+    private int currentSize;
+
+    public PartialArray()
+    {
+        // initialise instance variables
+        this.values = new int[100];
+        
+        for( this.currentSize = 0;
+             this.currentSize < 10;
+             this.currentSize++ )
+        {
+            this.values[this.currentSize] =
+                    this.currentSize * this.currentSize;
+        }
+    }
+
+}
